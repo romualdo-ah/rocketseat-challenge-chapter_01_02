52b7732aca42f403b6464d3a686c64f6de48204d
rooky6 <rooky6@users.noreply.github.com>
2021-06-18 14:42:20 -0300

    Modifiying api baseURL to production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:3333"
+      : 'https://my-json-server.typicode.com/romualdo-ah/rocketseat-challenge-chapter_01_02/server.json',
 });
