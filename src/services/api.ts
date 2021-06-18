d1431f6388f6c07299b6c1dd470179c2224befb6
rooky6 <rooky6@users.noreply.github.com>
2021-06-18 20:57:49 -0300

    Updating api baseURL to production url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NODE_ENV !== "production"?'http://localhost:3333':"https://epic-allen-0e21ba.netlify.app:3333",
+  baseURL: process.env.NODE_ENV !== "production"?'http://localhost:3333':"https://rocketseat-chapter-01-02-api.herokuapp.com/",
 });
